665833f4ace0558b3271c31e803e971907dde3f4
Kaan Gecü <kaan.gecu@d-teknoloji.com.tr>
2021-12-05 17:51:13 +0300

    gave the api url

--- dashboard/src/api/consts.js
+++ dashboard/src/api/consts.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const client = (url) => axios.create({baseURL:url})
 
-export const api_url =  process.env.API_URL || "http://localhost:5000/api"
+export const api_url =  "https://perfanalytics-api-kg.herokuapp.com/api"
 
