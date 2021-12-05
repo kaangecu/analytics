a59ed5341c741e3a17bd83fb63ca465bcff21793
Kaan Gecü <kaan.gecu@d-teknoloji.com.tr>
2021-12-05 16:53:04 +0300

    dashboard docker file

--- dashboard/src/api/consts.js
+++ dashboard/src/api/consts.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const client = (url) => axios.create({baseURL:url})
 
-export const api_url = "http://localhost:5000/api"
+export const api_url =  process.env.API_URL || "http://localhost:5000/api"
 
